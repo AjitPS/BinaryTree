b60cd864c7211d6b36e5e44825179cb712df25de
Ajit Singh <ajitprataps@yahoo.com>
2014-10-28 21:58:33 +0000

    commit1 BinaryTree

--- src/bintree/BinaryTreeDemo.java
+++ src/bintree/BinaryTreeDemo.java
@@ -7,8 +7,8 @@ public class BinaryTreeDemo {
 
   /* --Node--
    The binary tree is built using this nested node class. Each node stores one data element, and has left and right
-   sub-tree pointer which may be null. The node is a "dumb" nested class -- we just use it for
-   storage; it does not have any methods. */
+   sub-tree pointer which may be null. The node is a "dumb" nested class -- we just use it for storage; it does not have any 
+  methods. */
   private static class Node {
     Node left;
     Node right;
